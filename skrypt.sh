eb084607472341eb0f7896df6ca7e7fdd7ce31d0
Kamil <kamilkotkowski17@gmail.com>
2022-04-21 19:37:58 +0200

    Czwarty myslnik

--- skrypt.sh
+++ skrypt.sh
@@ -6,9 +6,16 @@ then
 echo $dzisiaj
 fi
 
-if [ "$1" == "--logs" ];
+if [ "$1" == "--logs" ] && [ -n "$2" ];
 then
-b=100
+for i in $(seq $2)
+do
+	echo log$i >> log$i.txt
+	echo $0 >> log$i.txt
+	echo $dzisiaj >> log$i.txt
+done
+elif [ "$1" == "--logs" ];
+then
 for i in $(seq 100)
 do
 	echo log$i >> log$i.txt
@@ -16,4 +23,3 @@ do
 	echo $dzisiaj >> log$i.txt
 done
 fi
-
