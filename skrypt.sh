882377f2e7335163cb26444a9719910cd6a6b930
Kamil <kamilkotkowski17@gmail.com>
2022-04-21 19:50:26 +0200

    Zaczecie Projekt cz2.

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ "$1" == "--help" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
 echo "Dostepne opcje:  "
 echo "--date wyswietla aktualna date"
 echo "--logs LICZBA tworzy LICZBA plikow o nazwie logLICZBA.txt i wpisuje do nich dane"
 echo "--logs jak wyzej ale tworzy 100 plikow"
 fi
 dzisiaj=$(date)
-if [ "$1" == "--date" ]; 
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; 
 then
 echo $dzisiaj
 fi
 
-if [ "$1" == "--logs" ] && [ -n "$2" ];
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ] && [ -n "$2" ];
 then
 for i in $(seq $2)
 do
@@ -20,7 +20,7 @@ do
 	echo $0 >> log$i.txt
 	echo $dzisiaj >> log$i.txt
 done
-elif [ "$1" == "--logs" ];
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ];
 then
 for i in $(seq 100)
 do
