0c1c8480d6255e015bf6d57b31162eeefc61a3b7
Kamil <kamilkotkowski17@gmail.com>
2022-04-21 19:19:01 +0200

    Trzeci myslnik

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,15 @@ if [ "$1" == "--date" ];
 then
 echo $dzisiaj
 fi
+
+if [ "$1" == "--logs" ];
+then
+b=100
+for i in $(seq 100)
+do
+	echo log$i >> log$i.txt
+	echo $0 >> log$i.txt
+	echo $dzisiaj >> log$i.txt
+done
+fi
+
