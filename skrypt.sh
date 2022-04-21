8bb6f866a7204933708970fc662cae8106c42dd2
Kamil <kamilkotkowski17@gmail.com>
2022-04-21 19:42:18 +0200

    Piaty myslnik

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$1" == "--help" ]; then
+echo "Dostepne opcje:  "
+echo "--date wyswietla aktualna date"
+echo "--logs LICZBA tworzy LICZBA plikow o nazwie logLICZBA.txt i wpisuje do nich dane"
+echo "--logs jak wyzej ale tworzy 100 plikow"
+fi
 dzisiaj=$(date)
 if [ "$1" == "--date" ]; 
 then
